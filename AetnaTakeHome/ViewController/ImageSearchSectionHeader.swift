104644b2ee0ebb8b3c81ed8ef58cd8839a756b4a
Joe Harasz <j30h30@github.com>
2021-11-24 13:15:39 -0500

    Added section header for welcome message and search results

---
+++ AetnaTakeHome/ViewController/ImageSearchSectionHeader.swift
@@ -0,0 +1,12 @@
+//
+//  ImageSearchSectionHeader.swift
+//  AetnaTakeHome
+//
+//  Created by Joe H on 11/24/21.
+//
+
+import UIKit
+
+class ImageSearchSectionHeader: UICollectionReusableView {
+    @IBOutlet weak var sectionHeaderlabel: UILabel!
+}
